2f10ee1b5102d6ce2bcb1024acf0c1b4930caa72
EduardoIvorra <62476207+EduardoIvorra@users.noreply.github.com>
2021-04-15 10:03:11 +0200

    Creacion pagina modificacion y edicion

--- practica1/practica1/src/main/java/es/sd/practica1/Repositorios/RepositorioCultivo.java
+++ practica1/practica1/src/main/java/es/sd/practica1/Repositorios/RepositorioCultivo.java
@@ -1,9 +1,11 @@
 package es.sd.practica1.Repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.sd.practica1.Entidades.Cultivo;
 
 public interface RepositorioCultivo extends JpaRepository<Cultivo,Long>{ 
-    
+    Optional<Cultivo> findById(Long id);
 }
