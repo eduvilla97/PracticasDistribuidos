16eed12fc36194e4e8741622fcce55efa1d45ce2
JGBURJC <guillobermejo@gmail.com>
2021-04-15 09:55:07 +0200

    Cambios en producto

--- practica1/practica1/src/main/java/es/sd/practica1/Controladores/ControladorProducto.java
+++ practica1/practica1/src/main/java/es/sd/practica1/Controladores/ControladorProducto.java
@@ -1,15 +1,67 @@
 package es.sd.practica1.Controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import es.sd.practica1.Entidades.Producto;
+import es.sd.practica1.Servicios.ServicioProducto;
 
 @Controller
 public class ControladorProducto {
+    @Autowired
+    private ServicioProducto productoServicio;
 
     @GetMapping(value="/productos")
-    public String productos(Model model) {
+    public String cultivos(Model model) {
+        List<Producto> prueba =  productoServicio.findAll();
+        model.addAttribute("listaProductos", productoServicio.findAll());
         return "productos";
     }
-    
+
+    @RequestMapping(value="/editarProductos/{id}", method=RequestMethod.GET)
+    public String editarProducto(@PathVariable Long id, String nombre, String descripcion, int plazoReentrada, int plazoRecoleccion) {
+        Producto producto = productoServicio.findById(id);
+        String strplazoReentrada= String.valueOf(plazoReentrada);
+        String strplazoRecoleccion =  String.valueOf(plazoRecoleccion);
+        if(nombre!= null){
+            producto.setNombre(nombre);
+        }
+        producto.setDescripcion(descripcion);
+
+        if(strplazoReentrada.isBlank()){
+            producto.setPlazoReentrada(0);
+        }else{
+            producto.setPlazoReentrada(plazoReentrada);
+        }
+
+        if(strplazoRecoleccion.isBlank()){
+            producto.setPlazoRecoleccion(0);
+        }else{
+           producto.setPlazoRecoleccion(plazoRecoleccion);
+        }    
+        return "exito";
+    }
+
+    @GetMapping(value="/formularioNuevoProducto")
+    public String formularioNuevoCultivo(Model model) {
+        return "formularioProducto";
+    }
+
+    @RequestMapping(value="/addNuevoProducto", method=RequestMethod.GET)
+    public String addNuevoProducto(String nombre, String descripcion, int plazoReentrada, int plazoRecoleccion) {
+        Producto nuevoCultivo = new Producto();
+        nuevoCultivo.setNombre(nombre);
+        nuevoCultivo.setDescripcion(descripcion);
+        nuevoCultivo.setPlazoReentrada(plazoReentrada);
+        nuevoCultivo.setPlazoRecoleccion(plazoRecoleccion);
+        productoServicio.save(nuevoCultivo);
+        return "exito";
+    }
 }
--- practica1/practica1/src/main/java/es/sd/practica1/Servicios/ServicioProducto.java
+++ practica1/practica1/src/main/java/es/sd/practica1/Servicios/ServicioProducto.java
@@ -1,8 +1,31 @@
 package es.sd.practica1.Servicios;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.sd.practica1.Entidades.Producto;
+import es.sd.practica1.Repositorios.RepositorioProducto;
 
 @Service
 public class ServicioProducto {
+    @Autowired
+    private RepositorioProducto repositorio;
+
+    public void save(Producto nuevoProducto) {
+        repositorio.save(nuevoProducto);
+    }
+
+    public List<Producto> findAll() {
+        return repositorio.findAll();
+    }
+
+    public Producto findById(long id) {
+        Optional<Producto> productoInDB = repositorio.findById(id);
+        if(productoInDB.isPresent()) return productoInDB.get();
+        return null;
+    }
     
 }
